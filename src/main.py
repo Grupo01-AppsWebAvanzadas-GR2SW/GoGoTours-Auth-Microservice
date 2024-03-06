8b79e8f410f1bd5dd5aadf5bafd0187de7bfa4be
Andrés Ponce <andres.ponce@epn.edu.ec>
2024-03-06 18:28:26 -0500

    update: docker

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 
 from google.cloud.firestore import AsyncClient
 
-from application.auth.repositories.users_repository_async import UsersRepositoryAsync
-from application.auth.services.login_service_async import LoginServiceAsync
-from application.auth.services.reset_password_service_async import ResetPasswordServiceAsync
-from application.auth.services.signup_service_async import SignupServiceAsync
-from infrastructure.firebase.auth.repositories.firestore_users_repository_async import FirestoreUsersRepositoryAsync
-from infrastructure.firebase.config.config import initialize_firebase, get_firestore_async
-from infrastructure.services.auth.default_login_service_async import DefaultLoginServiceAsync
-from infrastructure.services.auth.default_reset_password_service import DefaultResetPasswordServiceAsync
-from infrastructure.services.auth.default_signup_service_async import DefaultSignupServiceAsync
-from webapi.auth_routes import auth_router
+from src.application.auth.repositories.users_repository_async import UsersRepositoryAsync
+from src.application.auth.services.login_service_async import LoginServiceAsync
+from src.application.auth.services.reset_password_service_async import ResetPasswordServiceAsync
+from src.application.auth.services.signup_service_async import SignupServiceAsync
+from src.infrastructure.firebase.auth.repositories.firestore_users_repository_async import FirestoreUsersRepositoryAsync
+from src.infrastructure.firebase.config.config import initialize_firebase, get_firestore_async
+from src.infrastructure.services.auth.default_login_service_async import DefaultLoginServiceAsync
+from src.infrastructure.services.auth.default_reset_password_service import DefaultResetPasswordServiceAsync
+from src.infrastructure.services.auth.default_signup_service_async import DefaultSignupServiceAsync
+from src.webapi.auth_routes import auth_router
 
 initialize_firebase("/etc/secrets/firebase-credentials.json")
 load_dotenv("src/.env")
