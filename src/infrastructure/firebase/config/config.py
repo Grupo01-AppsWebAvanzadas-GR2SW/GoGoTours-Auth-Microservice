9fc41b956ff3c3258889b8a336d8355fc16816e7
Andrés Ponce <andres.ponce@epn.edu.ec>
2024-03-06 22:50:25 -0500

    refactor: credentials from env

--- src/infrastructure/firebase/config/config.py
+++ src/infrastructure/firebase/config/config.py
@@ -1,13 +1,17 @@
+import json
+import os
 from typing import Optional
 import firebase_admin
 from firebase_admin import credentials, firestore_async
 from google.cloud.firestore import AsyncClient
 
 
-def initialize_firebase(credentials_file_path: str | None = None) -> None:
+def initialize_firebase(credentials_file_path: Optional[str] = None) -> None:
     if credentials_file_path is None:
+        credentials_json = json.loads(os.getenv("FIREBASE_CONFIG"));
+        cred = credentials.Certificate(credentials_json)
         if not firebase_admin._apps:
-            firebase_admin.initialize_app()
+            firebase_admin.initialize_app(cred)
         return
 
     cred = credentials.Certificate(credentials_file_path)
